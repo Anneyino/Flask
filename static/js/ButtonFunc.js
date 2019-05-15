7500adcbaa4bae58febc9cdf6adf78e1788492da
CCIyx <410954231@qq.com>
2019-05-15 09:12:34 +0800

    弹窗的添加

--- static/js/ButtonFunc.js
+++ static/js/ButtonFunc.js
@@ -1,6 +1,8 @@
 var btnStudy = document.getElementsByClassName('btn')[0];
 var btnSports = document.getElementsByClassName('btn')[1];
 var btnFriends = document.getElementsByClassName('btn')[2];
+var div = document.getElementById('background');
+var close = document.getElementById('close-button');
 
 var mouseX = window.innerWidth / 2;
 var mouseY = window.innerHeight / 2;
@@ -21,5 +23,15 @@ btnFriends.addEventListener('mouseup', function(e){
 	mouseX = e.clientX;
 	mouseY = e.clientY;
 	
-
+	div.style.display = "block";
 });
+
+close.onclick = function close() {
+	div.style.display = "none";
+}
+ 
+window.onclick = function close(e) {
+	if (e.target == div) {
+		div.style.display = "none";
+	}
+}
