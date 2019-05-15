062307f95a0ed3dbe58ed4626a426a1255741f04
CCIyx <410954231@qq.com>
2019-05-15 11:47:24 +0800

    完善

--- static/js/study.js
+++ static/js/study.js
@@ -159,3 +159,21 @@ function homeClick(){
 function collectClick(){
 
 }
+
+var btn = document.getElementById('open_btn');
+var div = document.getElementById('background');
+var close = document.getElementById('close-button');
+ 
+btn.onclick = function show() {
+  div.style.display = "block";
+}
+ 
+close.onclick = function close() {
+  div.style.display = "none";
+}
+ 
+window.onclick = function close(e) {
+  if (e.target == div) {
+    div.style.display = "none";
+  }
+}
